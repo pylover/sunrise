36909ecb2d2e446e00fb0633232590ca160d633a
pylover <vahid.mardani@gmail.com>
2020-07-01 14:50:02 +0430

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+import os.path
+import re
+
+
+dependencies = [
+]
+
+
+setup(
+    name='sunrise',
+    version='0.1.0',
+    author='Vahid Mardani',
+    author_email='vahid.mardani@gmail.com',
+    url='http://github.com/pylover/sunrise',
+    description='A simple persian text-bot.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # This is important!
+    install_requires=dependencies,
+    packages=find_packages(exclude=['tests']),
+    license='MIT',
+    # TODO: Add calssifiers.
+)
