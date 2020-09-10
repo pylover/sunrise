bb01dd757404a3998b875404df2ecff62f062850
hamid <h.hami314@yahoo.com>
2020-09-10 11:18:27 +0430

    add subtraction styles to calculator class. Closes #11

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,5 +4,7 @@ from sunrise import do
 def test_parser():
     assert '6' == do('2 + 4')
     assert '6' == do('Add 2 by 4')
+    assert '2' == do('subtract 2 from 4')
+    assert '2' == do('4 - 2')
 
 
